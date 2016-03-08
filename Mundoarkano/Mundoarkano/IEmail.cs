e114960df8ed8e7a09fd16e81810b4086b02ff6d
ramiroDevayr <rsala@devayr.com>
2016-03-08 19:58:16 -0300

    agregado interface

---
+++ Mundoarkano/Mundoarkano/IEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mundoarkano
+{
+    interface IEmail
+    {
+        void EnviarEmail(string remitente, string destinatario, string asunto, string cuerpo);
+    }
+}
